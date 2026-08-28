816b8c4ef3c9a4d66dcab94a7106069ee04495ad
Paulo Henrique Martins <paulonater2016@gmail.com>
2026-08-28 12:35:17 -0300

    confere e desconta a quantidade disponível da oferta

--- app/actions/resgate.ts
+++ app/actions/resgate.ts
@@ -2,21 +2,36 @@
 
 import { prisma } from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
-import { success } from "zod"
 
 export default async function criarResgate(userId: string, ofertaId: string) {
 
   const codigoPinGerado = Math.floor(1000 + Math.random() * 9000).toString()
 
-  const novoResgate = await prisma.resgate.create({
-    data: {
-      userId: userId,
-      ofertaId: ofertaId,
-      codigoPin: codigoPinGerado,
-      status: "PENDENTE" 
-    }
-  })
+  const novoResgate = await prisma.$transaction(async (tx) => {
+    const ofertaAtualizada = await tx.oferta.update({
+      where: {
+        id: ofertaId,
+        quantidade: {gt: 0}
+      },
+      data: {
+        quantidade: { decrement: 1 }
+      }
+    }).catch(() => null)
 
+
+    if(!ofertaAtualizada) {
+      throw new Error("Oferta esgotada")
+    }
+
+    return tx.resgate.create({
+      data: {
+        userId: userId,
+        ofertaId: ofertaId,
+        codigoPin: codigoPinGerado,
+        status: "PENDENTE"
+      }
+    })
+  })
   revalidatePath("/perfil")
   return novoResgate
 }

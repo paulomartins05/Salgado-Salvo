2f528de73b471051d35cb5413818e06112f2ae17
Paulo Henrique Martins <paulonater2016@gmail.com>
2026-09-01 15:43:57 -0300

    Atualizar o email, agora envia para o better-auth

--- app/actions/usuario.ts
+++ app/actions/usuario.ts
@@ -43,12 +43,20 @@ export async function atualizarPerfilUsuario(formData: FormData) {
         }
     })
 
+    if (email && email != session.user.email) {
+        await auth.api.changeEmail({
+            headers: reqHeaders,
+            body: {
+                newEmail: email,
+            }
+        })
+    }
+
     await prisma.user.update({
         where: {
             id: session.user.id
         },
         data: {
-            email: email,
             telefone: telefone,
             ...(cnpj && { cnpj }),
             ...(localizacao && { localizacao })
